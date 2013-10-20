4d5e29216fc763e9aad5f5377c574cd94392b727
Keerthana Baskaran <keerthana_baskaran@htc.com>
2013-10-20 03:08:08 -0700

    missed to check in tabs adapter

---
+++ src/com/gbox/adapters/TabsPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.gbox.adapters;
+
+import com.example.gbox.CloseFriendFragment;
+import com.example.gbox.JustEveryoneFragment;
+import com.example.gbox.UpcomingBirthdayFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+
+	public TabsPagerAdapter(FragmentManager fm) {
+		super(fm);
+	}
+
+	@Override
+	public Fragment getItem(int index) {
+		switch (index) {
+		case 0:
+			return new UpcomingBirthdayFragment();
+		case 1:
+			return new CloseFriendFragment();
+		case 2:
+			return new JustEveryoneFragment();
+		}
+
+		return null;
+
+	}
+
+	@Override
+	public int getCount() {
+		// get item count - equal to number of tabs
+		return 3;
+
+	}
+
+}
